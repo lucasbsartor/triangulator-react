3805e1feb4428077d3abf468a2137c2c727f7753
Lucas de Bona Sartor <lucas.bsartor@pagar.me>
2024-02-15 19:27:09 -0300

    Adds react hook form

--- src/components/TriangleCalculator.tsx
+++ src/components/TriangleCalculator.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { z } from 'zod'
 import {
   Triangle,
   getTriangleType,
@@ -9,28 +10,55 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from './ui/card'
-import { Button } from './ui/button'
-import { Input } from './ui/input'
 import { useToast } from './ui/use-toast'
 import Results from './Results'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from './ui/form'
+import { Input } from './ui/input'
+import { Button } from './ui/button'
+
+const triangleFormSchema = z.object({
+  sideA: z.coerce
+    .number()
+    .int()
+    .positive({ message: 'Esse campo precisa ser um número maior que 0' }),
+  sideB: z.coerce
+    .number()
+    .int()
+    .positive({ message: 'Esse campo precisa ser um número maior que 0' }),
+  sideC: z.coerce
+    .number()
+    .int()
+    .positive({ message: 'Esse campo precisa ser um número maior que 0' }),
+})
 
 const TriangleCalculator = () => {
-  const [sideA, setsideA] = useState<number>(0)
-  const [sideB, setsideB] = useState<number>(0)
-  const [sideC, setsideC] = useState<number>(0)
   const [triangle, setTriangle] = useState<Triangle | null | undefined>()
   const { toast } = useToast()
 
-  const handleSubmit = (e: React.SyntheticEvent) => {
-    e.preventDefault()
-    getTriangleInfo()
-  }
+  const triangleForm = useForm<z.infer<typeof triangleFormSchema>>({
+    resolver: zodResolver(triangleFormSchema),
+    defaultValues: {
+      sideA: 0,
+      sideB: 0,
+      sideC: 0,
+    },
+  })
 
-  const getTriangleInfo = () => {
+  const onSubmit = (values: z.infer<typeof triangleFormSchema>) => {
+    const { sideA, sideB, sideC } = values
     const triangleType = getTriangleType(sideA, sideB, sideC)
     const triangleAngles = getTriangleAngles(sideA, sideB, sideC)
 
@@ -58,32 +86,64 @@ const TriangleCalculator = () => {
             Calcule o angulo de um triangulo usando os seus vertices
           </CardDescription>
         </CardHeader>
-        <form onSubmit={handleSubmit}>
-          <CardContent>
-            <div className="grid w-full items-center gap-4">
-              <Input
-                type="number"
-                placeholder="Lado 1"
-                onChange={(e) => setsideA(e.target.valueAsNumber)}
+        <CardContent>
+          <Form {...triangleForm}>
+            <form
+              onSubmit={triangleForm.handleSubmit(onSubmit)}
+              className="space-y-2"
+            >
+              <FormField
+                control={triangleForm.control}
+                name="sideA"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Lado A</FormLabel>
+                    <FormControl>
+                      <Input type="number" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      Esse é um lado do triangulo
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
               />
-              <Input
-                type="number"
-                placeholder="Lado 2"
-                onChange={(e) => setsideB(e.target.valueAsNumber)}
+              <FormField
+                control={triangleForm.control}
+                name="sideB"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Lado B</FormLabel>
+                    <FormControl>
+                      <Input type="number" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      Esse é um lado do triangulo
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
               />
-              <Input
-                type="number"
-                placeholder="Lado 3"
-                onChange={(e) => setsideC(e.target.valueAsNumber)}
+              <FormField
+                control={triangleForm.control}
+                name="sideC"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Lado C</FormLabel>
+                    <FormControl>
+                      <Input type="number" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      Esse é um lado do triangulo
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
               />
-            </div>
-          </CardContent>
-          <CardFooter className="grid w-full">
-            <Button className="btn" onClick={getTriangleInfo}>
-              Testar
-            </Button>
-          </CardFooter>
-        </form>
+              <Button type="submit">Calcular</Button>
+            </form>
+          </Form>
+        </CardContent>
       </Card>
       <br />
       {triangle && <Results triangle={triangle} />}
